4314ddcfa6210abe4c14295bd93f90beea88600b
Denis Zykov <deniszykov@gmail.com>
2023-05-21 13:32:45 +0200

    upd doc config

--- docs/conf.py
+++ docs/conf.py
@@ -43,7 +43,7 @@ templates_path = ['_templates']
 source_suffix = '.rst'
 
 # The master toctree document.
-master_doc = 'README'
+master_doc = 'index'
 
 # General information about the project.
 project = 'Charon'
